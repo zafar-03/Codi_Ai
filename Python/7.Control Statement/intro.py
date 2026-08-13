4d3e9b2f6cd1962dd4c4f054b3fde9ac4b935cbb
zafar-03 <zfnanshabhai@gmail.com>
2026-08-13 13:34:19 +0530

    Conditional Control Statement

---
+++ Python/7.Control Statement/intro.py
@@ -0,0 +1,51 @@
+# num1 = -345
+'''
+if(condition) :
+    statement
+'''
+# ================================================
+# if (num1 > 0) :
+#     print("Number +ve")
+# else:
+#     print("Number -ve")
+# ================================================
+
+# num1 = int(input("Enter the Value of N1 : "))
+# num2 = int(input("Enter the Value of N2 : "))
+
+# if (num1 > num2):
+#     print("Number1 is Greater")
+# else:
+#     print("Number2 is Greater")
+# ================================================
+
+# num1 = int(input("Enter the Value of N1 : "))
+
+# if(num1 % 3 == 0):
+#     print("Number is Divisible  by 3")
+# else:
+#     print("Number is not Divisible  by 3")
+# ================================================
+
+
+
+
+
+# Control Statement : 
+# there 3 ways.
+# 1. Condition Control Statement 
+    # if-else block
+
+# 2. Case Control Statement (X)
+# 3. Loop Control Statement 
+
+# ================================================
+
+# 1.WAP to check Given Data match with "Admin" or not.
+
+# 2.WAP to Check Given Number is Zero or Not.
+	
+# 3.Check voting eligibility based on age.
+
+# 4.WAP to Check Given Number is Natural or Not.
+
